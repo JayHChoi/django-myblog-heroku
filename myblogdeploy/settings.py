c58f97d0d3c39b1d6f7d02afc184b66feaadf771
JayHChoi <jaychoi1619@gmail.com>
2018-10-30 13:53:31 +0900

    dfdf

--- myblogdeploy/settings.py
+++ myblogdeploy/settings.py
@@ -21,15 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY', 'mf5_=meq3ffkj5t5xbngk2-69=(@utyuq^xmyg2$-!r6ytza7t')
-SECRET_KEY = 'mf5_=meq3ffkj5t5xbngk2-69=(@utyuq^xmyg2$-!r6ytza7t'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', 'mf5_=meq3ffkj5t5xbngk2-69=(@utyuq^xmyg2$-!r6ytza7t')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = os.environ.get('DEBUG', False)
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'jaychoi.net', '.herokuapp.com',]
+ALLOWED_HOSTS = ['127.0.0.1', 'jaychoi.net', '.herokuapp.com', ]
 
 
 # Application definition
