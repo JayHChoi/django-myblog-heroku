f3d478a216b9c853ab39abeb320d1cd2c924598b
JayHChoi <jaychoi1619@gmail.com>
2018-10-30 12:57:29 +0900

    django-heroku2

--- myblogdeploy/settings.py
+++ myblogdeploy/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'mf5_=meq3ffkj5t5xbngk2-69=(@utyuq^xmyg2$-!r6ytza7t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jaychoi.net',]
 
 
 # Application definition
