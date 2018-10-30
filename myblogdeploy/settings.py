8b73b9ec149328a772e73e4a62c8d42c7e30ea9b
JayHChoi <jaychoi1619@gmail.com>
2018-10-30 13:55:23 +0900

    dfdf

--- myblogdeploy/settings.py
+++ myblogdeploy/settings.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
-import os
 import django_heroku
+import os
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,4 +130,4 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), {'allowed_hosts': False})
